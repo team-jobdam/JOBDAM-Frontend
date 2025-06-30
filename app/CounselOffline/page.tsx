0d62788a6c69ba251a421e71a94a5b096648048f
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-07-01 00:12:12 +0900

    page.tsx설정 및 path설정

---
+++ app/CounselOffline/page.tsx
@@ -0,0 +1,10 @@
+'use client';
+
+import React from 'react';
+import { CounselAccountOffline } from '@/container/Counsel';
+
+const CounselOfflinePage: React.FC = () => {
+  return <CounselAccountOffline />;
+};
+
+export default CounselOfflinePage;
