2ff0cd3b43604c18c980077a9cab427822b7b4e2
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-06-30 23:13:49 +0900

    setting/tailwind

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,7 @@ const config: Config = {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primaryGreen: "#02C551",
       },
     },
   },
