29ab7f95dcb929d3e8ce52b53f9b5140895058e8
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-07-02 01:58:58 +0900

    버튼 안에 폰트 지정

--- app/components/category.tsx
+++ app/components/category.tsx
@@ -9,7 +9,7 @@ interface CategoryProps {
 
 export function Category({ value, selected = false, onClick }: CategoryProps) {
   return (
-    <button className={`px-4 py-2 border rounded-[4px] font-pretendard ${selected ? "text-primary border-primary" : "text-black border-black"}`} onClick={onClick}>
+    <button className={`px-4 py-2 border rounded-[4px] text-[1rem] font-[400] font-pretendard ${selected ? "text-primary border-primary" : "text-black border-black"}`} onClick={onClick}>
       {value}
     </button>
   );
