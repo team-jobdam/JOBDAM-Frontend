12ca5d602ee1fb4e28ccdbb8096b1fb7956cd7e3
tlrdmsEjrqhRdl <s24030@gsm.hs.kr>
2025-07-02 01:45:43 +0900

    상담 타이틀 섹션 나누기 및 제목

--- app/container/Counsel/CounselAccount.tsx
+++ app/container/Counsel/CounselAccount.tsx
@@ -1,10 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
+import { Category } from "@/components";
 
-export function CouncelAccount() {
+export function CounselAccount() {
+  const [selectedCategory, setSelectedCategory] = useState<string | null>(null);
+
   return(
     <>
-    <div id="container" className="w-[100vw] h-[100h] bg-white flex flex-col items-center">
+    <div id="container" className="w-[100vw] h-[100vh] bg-white flex flex-col items-center">
       
+      {/* 온오프라인 선택 */}
+      <div id="categoryContainer" className="w-[46%] h-auto mt-16 flex flex-col ">
+        <p id="categoryTitle" className="text-black text-[1.6rem] font-700 font-pretendard">상담 종류 선택을 도와드릴게요.</p>
+        <p id="categoryDescription">주제와 관련되 카테고리를 설정해주세요.</p>
+
+        <div id="categoryList" className="flex flex-row gap-[2vw]">
+        <Category value="온라인" selected={selectedCategory === "온라인"} onClick={() => setSelectedCategory("온라인")} />
+        <Category value="오프라인" selected={selectedCategory === "오프라인"} onClick={() => setSelectedCategory("오프라인")} />
+        </div>
+      </div>
+
+      {/*온라인 or 오프라인 정보 입력*/}
+      <div id="contentContainer">
+
+      </div>
+
+      {/* 제출&취소 버튼 */}
+      <div id="buttonContainer"></div>
     </div>
     </>
   )
